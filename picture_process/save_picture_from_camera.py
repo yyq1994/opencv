fb58e423553d8bb554735e2c3f702125810db185
yyq1994 <45325354+yyq1994@users.noreply.github.com>
2020-02-14 22:32:34 +0800

    Add files via upload

---
+++ picture_process/save_picture_from_camera.py
@@ -0,0 +1,42 @@
+import time
+import cv2 as cv
+import os
+import shutil
+import time
+cap = cv.VideoCapture(0)
+# 保存图片10张
+
+# 图片个数
+i = 0
+
+# 根据当前时间，创建图片文件夹
+base_name = str(time.time())
+os.mkdir(base_name)
+
+# 摄像头是否打开
+while cap.isOpened():
+    if i == 10:     #10张图片就退出
+        break
+    else:
+        i += 1
+
+    # flag:是否读取成功
+    flag,frame = cap.read()
+
+    #设置图片名称
+    file_name = base_name+'/frame'+str(i)+'.jpg'
+
+    # 显示图片
+    cv.imshow(file_name,frame)
+
+    # 保存图片到文件夹
+    cv.imwrite(file_name,frame)
+
+    # 提示图片保存成功
+    print('第{} 张图片保存成功'.format(i))
+
+    # 每1秒保存一张图片
+    time.sleep(1)
+
+
+print('\n10张图片保存完成')
